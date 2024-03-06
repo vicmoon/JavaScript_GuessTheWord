1470023ef71b30ec409791bc0d6443831d0db6ee
Munteanu <munteanuvictoria1@gmail.com>
2024-03-06 08:35:34 +0100

    add script

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const inputs = document.querySelector(".inputs");
 const resetBTN = document.querySelector(".reset-btn");
 const hint = document.querySelector(".hint span");
 const typingInput = document.querySelector('.typing-input');
-
+let word; 
 
 
 function randomWord(){
     // get random object from wordlist  
 
     let randomObject = wordList[Math.floor(Math.random()* wordList.length)];
-    let word = randomObject.word;
+    word = randomObject.word;
     
     hint.innerHTML =  randomObject.hint;
 
@@ -27,8 +27,17 @@ function initGame(e){
 
     //Getting user input 
     let key = e.target.value; 
-    if(key.match())
-    console.log(key);
+    if(key.match(/^[a-zA-Z]+$/)){
+        console.log(key);
+        if(word.includes(key)){  // if user input is part of the word 
+         for(i=0; i < word.length; i++){
+          console.log('letter found');
+         }
+        }else{
+          console.log('letter not found');
+        }
+    }
+    
 
 }
 resetBTN.addEventListener('click', randomWord);
